162e9d17348e53a071cd618b928df73c3904c6fa
ffdn <work.afifudin@gmail.com>
2022-11-11 14:03:36 +0700

    Fix Login Service

--- services/login_service.php
+++ services/login_service.php
@@ -7,22 +7,26 @@ $role = $form['role'];
 
 
 if (empty($username) || empty($password) || empty($role)) {
-    header('Location: ../login.php');
-    return;
-} else if (!in_array($role, ['user', 'kurir', 'admin'])) {
-    header('Location: ../login.php');
-    return;
+  header('Location: ../login.php');
+  return;
+} else if (!in_array($role, ['user', 'driver', 'admin'])) {
+  header('Location: ../login.php');
+  return;
 }
-
-$role = $role . 's';
+if ($role != 'admin') {
+  $role = $role . 's';
+}
 $data = mysqli_query($conn, "select * from $role where username='$username' ");
 $data = mysqli_fetch_array($data);
 if (empty($data)) {
-    header('Location: ../login.php');
-    return;
+  header('Location: ../login.php');
+  return;
 }
 if ($password != $data['password']) {
-    header('Location: ../login.php');
-    return;
+  header('Location: ../login.php');
+  return;
 }
+setcookie("username", $username, time() + (10 * 365 * 24 * 60 * 60), "/");
+setcookie("password", $password, time() + (10 * 365 * 24 * 60 * 60), "/");
+setcookie("role", $role, time() + (10 * 365 * 24 * 60 * 60), "/");
 echo "<SCRIPT> alert('Login sukses');window.location='../index.php'</SCRIPT>";
