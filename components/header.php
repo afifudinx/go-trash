ceb7be2c3fc59ef86214c47a6173978871385b04
ZuniAmandaDewi <101152490+ZuniAmandaDewi@users.noreply.github.com>
2022-12-04 15:21:11 +0700

    Update header.php

--- components/header.php
+++ components/header.php
@@ -6,6 +6,15 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="assets/css/bootstrap.css">
+  <link rel="stylesheet" href="assets/css/home1.css">
+  <link rel="stylesheet" href="assets/css/home2.css">
+  <link rel="stylesheet" href="assets/css/home3.css">
+  <link rel="stylesheet" href="assets/css/home4.css">
+
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/home1.css">
+  <!-- Load fonts style after rendering the layout styles -->
+  <link rel="stylesheet" href="assets/css/home5.css">
+  
   <script src="assets/js/bootstrap.bundle.js"></script>
   <title>Go-Trash</title>
 </head>
